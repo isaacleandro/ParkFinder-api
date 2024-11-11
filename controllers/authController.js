ebf1725159f08227036134a3de907f16afa8384a
isaac leandro <isaac_k4ks@outlook.com>
2024-11-11 18:04:12 -0300

    modificado

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,52 @@
+import UserRepository from "../repositories/userRepository.js";
+import { hashPassword,comparePasswords } from "../utils/hash.js";
+
 const AuthController = {
-    login: (req, res) => {
-        res.send('POST /auth/login');
+    login: async (req, res) => {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).send('Missing fields');
+        }
+
+        const user = await UserRepository.finderByEmail(email);
+
+        if (!user ) {
+            return res.status(404).send('User not found');
+        }
+
+        const isValidPassword = await comparePasswords(password, user.password);
+
+        if (!isValidPassword) {
+            return res.status(401).send('Invalid password');
+        }
+
+        res.send({
+            email: user.email,
+            name: user.name
+        })
     },
-    register: (req, res) => {
-        res.send('POST /auth/register');
+    register:async (req, res) => {
+
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).send('Missing fields');
+        }
+
+        const hashedPassword = await hashPassword(password);
+
+        UserRepository.create({ 
+            name, 
+            email, 
+            password: hashedPassword 
+        });
+
+        res.send({
+            email,
+            name
+        })
     }
 }
 
--- routes/spacesRoutes.js
+++ routes/spacesRoutes.js
@@ -4,17 +4,26 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     // pegar vaga
-   res.send('GET /spaces');
+    res.send('GET /spaces');
 });
 
 router.post("/", (req, res) => {
-   // add vaga
-   res.send('POST /spaces');
+    // add vaga
+    res.send('POST /spaces');
 });
 
 router.delete("/:id", (req, res) => {
     //delete vaga
     res.send('DELETE /spaces');
 });
+
+router.post('/:id/occupy', (req, res) => {
+    //preencher vaga
+})
+
+router.post('/:id/fill-out', (req, res) => {
+    //desocupar vaga
+    res.send('POST /spaces/:id/fill-out');
+})
 
 export default router;
